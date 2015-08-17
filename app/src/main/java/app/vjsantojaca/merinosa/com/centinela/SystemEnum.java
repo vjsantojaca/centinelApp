90ea75042d63d60ab707d2a1eb614635cb12f187
vjsantojaca <vsantoja@onwhyon.com>
2015-08-17 11:50:59 +0200

    Enumeration with the message type to the system

---
+++ app/src/main/java/app/vjsantojaca/merinosa/com/centinela/SystemEnum.java
@@ -0,0 +1,13 @@
+package app.vjsantojaca.merinosa.com.centinela;
+
+/*
+* Developer Víctor Santoja
+ * Centinela App pertenece al proyecto Centinela
+*/
+public enum SystemEnum
+{
+    SHUTDOWN,
+    START,
+    DELETEAPP,
+    GPSOFF;
+}
