4c70d9642714f7a2a38b8372a7df1cccc9181be7
vjsantojaca <vsantoja@onwhyon.com>
2015-08-17 10:39:18 +0200

    Send to server the token of google cloud messaging

--- app/src/main/java/app/vjsantojaca/merinosa/com/centinela/services/gcm/RegistrationIntentService.java
+++ app/src/main/java/app/vjsantojaca/merinosa/com/centinela/services/gcm/RegistrationIntentService.java
@@ -3,19 +3,34 @@ package app.vjsantojaca.merinosa.com.centinela.services.gcm;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.iid.InstanceID;
 
-import app.vjsantojaca.merinosa.com.centinela.Constants;
-import app.vjsantojaca.merinosa.com.centinela.R;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-/**
- * Created by vsantoja on 14/08/15.
- */
+import app.vjsantojaca.merinosa.com.centinela.App;
+import app.vjsantojaca.merinosa.com.centinela.Constants;
+import app.vjsantojaca.merinosa.com.centinela.LoginActivity;
+import app.vjsantojaca.merinosa.com.centinela.Utils;
+import app.vjsantojaca.merinosa.com.centinela.volley.ServerStatusRequestObject;
+import app.vjsantojaca.merinosa.com.centinela.volley.VolleyS;
+
+/*
+* Developer Víctor Santoja
+ * Centinela App pertenece al proyecto Centinela
+*/
 public class RegistrationIntentService extends IntentService
 {
     private static final String TAG = RegistrationIntentService.class.getName();
@@ -36,11 +51,10 @@ public class RegistrationIntentService extends IntentService
                 // Initially this call goes out to the network to retrieve the token, subsequent calls
                 // are local.
                 InstanceID instanceID = InstanceID.getInstance(this);
-                String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+                String token = instanceID.getToken("877536983813", GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+                //String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
                 Log.i(TAG, "GCM Registration Token: " + token);
 
-
-                // TODO: Implement this method to send any registration to your app's servers.
                 sendRegistrationToServer(token);
 
 
@@ -71,6 +85,38 @@ public class RegistrationIntentService extends IntentService
      */
     private void sendRegistrationToServer(String token)
     {
+        VolleyS volleyS = App.getVolley();
+        String url = Constants.URL_SERVER + Constants.PATH_GCM;
+        JSONObject object = new JSONObject();
+        try {
+            object.put("regId", token);
+            object.put("idUser", Utils.getNumberPhone());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
+        ServerStatusRequestObject serverStatusRequest = new ServerStatusRequestObject(
+                Request.Method.POST,
+                url,
+                null,
+                object.toString(),
+                new Response.Listener<Integer>() {
+                    @Override
+                    public void onResponse(Integer response) {
+                        if( response == 200 ) {
+                            Log.d(TAG, "Guardado correctamente en el servidor");
+                            Intent intent = new Intent(App.getAppContext(), LoginActivity.class);
+                            startService(intent);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "Error Respuesta: " + error);
+                    }
+                }
+        );
+        volleyS.getRequestQueue().add(serverStatusRequest);
     }
 }
